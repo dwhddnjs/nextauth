a691af297f4d10d776acca836c7b8216ecf1d536
dwhddnjs <per1215@naver.com>
2024-03-03 12:47:39 +0900

    카드 컴포넌트 추가

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { LoginForm } from "@/components/auth/login-form"
+import React from "react"
+
+const LoginPage = () => {
+  return <LoginForm />
+}
+
+export default LoginPage
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+interface LoginButtonProp {
+  children: React.ReactNode
+  mode?: "modal" | "redirect"
+  asChild?: boolean
+}
+
+export const LoginButton = ({ children, mode, asChild }: LoginButtonProp) => {
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push("/auth/login")
+  }
+
+  if (mode === "modal") {
+    return <span>TODO</span>
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  )
+}
