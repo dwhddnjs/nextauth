e6fbb295d370f93423960da8d534f44d5f2918a7
dwhddnjs <per1215@naver.com>
2024-03-06 01:57:01 +0900

    authjs user 생성 기능 추가

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,9 @@
 
 import { LoginSchema } from "@/schemas"
 import * as z from "zod"
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedField = LoginSchema.safeParse(values)
@@ -10,5 +13,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields" }
   }
 
-  return { success: "Email sent!" }
+  const { email, password } = validatedField.data
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" }
+        default:
+          return { error: "Something went wrong!" }
+      }
+    }
+
+    throw error
+  }
 }
--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server"
 
+import { getUserByEmail } from "@/data/user"
+import { db } from "@/lib/db"
 import { RegisterSchema } from "@/schemas"
+import bcrypt from "bcryptjs"
 import * as z from "zod"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,6 +12,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedField.success) {
     return { error: "Invalid fields" }
   }
+
+  const { email, password, name } = validatedField.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "Email already in use!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
 
   return { success: "Email sent!" }
 }
