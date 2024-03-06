5323ed7cbb6e24028d8a7232939b77df6b0c2d51
dwhddnjs <per1215@naver.com>
2024-03-07 00:15:25 +0900

    nextauth session 추가

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-
+import { getUserById } from "./data/user"
 import { db } from "@/lib/db"
+import { UserRole, User } from "@prisma/client"
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +11,37 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id as string)
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false
+    //   }
+    //   return true
+    // },
+    async session({ session, user, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
